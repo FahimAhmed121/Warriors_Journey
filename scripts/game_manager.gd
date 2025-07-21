3b894a932865fb1c81011fee47c2d20dc0476ce9
FahimAhmed121 <mdfahimahmedyasin@gmail.com>
2025-07-21 23:51:38 +0600

    Initial commit

---
+++ scripts/game_manager.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var score = 0
+@onready var score_label: Label = $ScoreLabel
+@onready var score_label_2: Label = $ScoreLabel2
+
+func add_point():
+	score += 1
+	score_label.text = "You collected " + str(score) + " coins."
+	score_label_2.text = "You collected " + str(score) + " coins."
